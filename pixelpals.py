ccfdacf4793728962621a8b5a4a37ab9bc1fd136
thomasm1248 <thomasm1248@gmail.com>
2023-10-19 14:14:37 -0600

    Add functions to convert between date and string

--- pixelpals.py
+++ pixelpals.py
@@ -20,6 +20,30 @@ pet = None
 # General purpose functions that will be used by other parts of the
 # program.
 
+def dateToText(date):
+    """
+    Convert a date to a string using the format: %Y%m%d%H%M%S%f
+
+    Args:
+        date (datetime): the date to be converted
+
+    Returns:
+        string: the string
+    """
+    return date.strftime("%Y%m%d%H%M%S%f")
+
+def textToDate(text):
+    """
+    Parse a date from a string using the format: %Y%m%d%H%M%S%f
+
+    Args:
+        text (string): the string to be parsed
+
+    Returns:
+        datetime: parsed date
+    """
+    return datetime.strptime(text, "%Y%m%d%H%M%S%f")
+
 #  #      ###   ###  ###  ###
 #  #     #   # #      #  #   #
 #  #     #   # #  ##  #  #
