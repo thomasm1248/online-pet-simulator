d0130552d251cb54c1dcb08c5fc816be604b15ac
thomasm1248 <thomasm1248@gmail.com>
2023-10-19 13:19:48 -0600

    Make a function to apply time to pet

--- pixelpals.py
+++ pixelpals.py
@@ -8,6 +8,20 @@
 #
 # Functions that handle the logic of the program.
 
+# Apply time to pet by calling the pet's tick method once per
+# minute of time.
+def simulateEffectOfTimeOnPet(pet, time):
+    """
+    Apply time to pet by calling the pet's tick method once per
+    minute of time that has passed.
+
+    Args:
+        pet (pet object): the pet
+        time (int): the minutes
+    """
+    for i in range(time):
+        pet.tick()
+
 # Event handlers
 #
 # Functions that will be called when GUI events take place.
