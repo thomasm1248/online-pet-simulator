96b42140cdc0ffa728cd782dda116e7159361b59
thomasm1248 <thomasm1248@gmail.com>
2023-10-19 13:07:22 -0600

    Create a basic outline of the python file

---
+++ pixelpals.py
@@ -0,0 +1,13 @@
+
+# Util functions
+#
+# General purpose functions that will be used by other parts of the
+# program.
+
+# Program logic
+#
+# Functions that handle the logic of the program.
+
+# Event handlers
+#
+# Functions that will be called when GUI events take place.
