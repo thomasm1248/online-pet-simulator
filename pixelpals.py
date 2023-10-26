ec6faa246aca1091fb8c6a2c75149a5ac9966fb6
thomasm1248 <thomasm1248@gmail.com>
2023-10-25 19:55:22 -0600

    Add functions to read and write save file

--- pixelpals.py
+++ pixelpals.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import json
 
 
 
@@ -10,8 +11,8 @@ from datetime import datetime
 #
 # Any global variables we need
 
-# The variable that will hold the pet object
-pet = None
+# State object
+state = None
 
 # Constants
 DATETIME_FORMAT = "%Y%m%d%H%M%S%f"
@@ -79,6 +80,55 @@ def simulateEffectOfTimeOnPet(pet, startTime, endTime):
     # Call the pet's tick method once for each minute
     for i in range(minutes):
         pet.tick()
+        # TODO: save a snapshot of pet stats each hour or so
+
+def readStateFromSaveFile():
+    """
+    Read the save file if there is one, and put the data in the global state
+    variable. Certain values will be converted to a form more useable by the
+    rest of the program. This function is designed be be run at the start of
+    the program.
+
+    Returns:
+        boolean: true if save file successfully read
+    """
+    # Read data from the file, return False upon failure
+    try:
+        file = open("pixelpalsave.json")
+        global state
+        state = json.load()
+        file.close();
+    except:
+        return False
+    # Instantiate a pet object
+    # TODO
+    # Convert dates to datetime objects
+    state.initial_date = textToDate(state.initial_date)
+    state.save_date = textToDate(state.save_date)
+    # Since there was a save file, return True
+    return True
+
+def saveStateToFile():
+    """
+    Save the current state to a save file. The current state can be found
+    in the global state variable. Certain values will be converted into a
+    format more JSON friendly. This function is designed be be run
+    multiple times throughout the execution of the program.
+    """
+    # Create a data object that will be converted to JSON
+    saveData = {}
+    # Store some of the simpler objects
+    saveData.initial_date = dateToText(state.initial_date)
+    saveData.save_date = datetime.now()
+    saveData.money = state.money
+    # Make a simpler copy of the pet object
+    saveData.pet = None # TODO
+    # Store the statistics
+    saveData.pet_stats = state.pet_stats
+    # Save the save data to a file
+    file = open("pixelpalsave.json", "w")
+    json.dump(saveData, file)
+    file.close()
 
 
 
